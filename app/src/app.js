78b4588b04a64bdaea8c5a73f0696d050d07e0a1
Darryl <darrylmbae01@gmail.com>
2025-11-25 01:33:56 +0300

    chore: add project skeleton and component structure

---
+++ app/src/app.js
@@ -0,0 +1,33 @@
+import {__jacJsx, __jacSpawn} from "@jac-client/utils";
+function Website() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["Hello World"])]);
+}
+function Auth() {
+  return __jacJsx("div", {}, []);
+}
+function Onboarding() {
+  return __jacJsx("div", {}, []);
+}
+function Dashboard() {
+  return __jacJsx("div", {}, []);
+}
+function app() {
+  document.body.style.margin = "0";
+  document.body.style.padding = "0";
+  document.body.style.backgroundColor = "black";
+  let preconnect1 = document.createElement("link");
+  preconnect1.rel = "preconnect";
+  preconnect1.href = "https://fonts.googleapis.com";
+  document.head.appendChild(preconnect1);
+  let preconnect2 = document.createElement("link");
+  preconnect2.rel = "preconnect";
+  preconnect2.href = "https://fonts.gstatic.com";
+  preconnect2.crossOrigin = "anonymous";
+  document.head.appendChild(preconnect2);
+  let fontLink = document.createElement("link");
+  fontLink.href = "https://fonts.googleapis.com/css2?family=Zalando+Sans+SemiExpanded:ital,wght@0,200..900;1,200..900&display=swap";
+  fontLink.rel = "stylesheet";
+  document.head.appendChild(fontLink);
+  return __jacJsx(Website, {}, []);
+}
+export { Auth, Dashboard, Onboarding, Website, app };
